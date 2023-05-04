dad67cff3c937fedbc2f03c9a63997c6c7cd4e74
Kuladeep561 <kuladeep561@gmail.com>
2023-05-04 15:58:50 +0200

    Refactoring

--- pylib/add_triples.py
+++ pylib/add_triples.py
@@ -1,39 +1,35 @@
-from rdflib import *
+from rdflib import*
 
-def add_triples(dmn_Abox, ns, classes, properties, inputs, outputs, i):
-    triples = [
-        (inputs['activity'][i], RDF.type, classes['activity']),
-        (inputs['activity'][i], properties['hasStartDate'], Literal(inputs.get('hasActivityStartDate')[i].strip('/"'), datatype=XSD.dateTime)),
-        (outputs['hasInspection'][i], RDF.type, classes['inspection']),
-        (outputs['hasInspection'][i], RDFS.comment, Literal(outputs.get('hasComments')[i], datatype=XSD.string)),
-        (outputs['hasInspection'][i], properties['hasCodeOfConduct'], ns['dmn'][outputs['hasCodeOfConduct'][i]]),
-        (outputs['hasResponsibleAgent'][i], RDF.type, classes['agent']),
-        (outputs['hasInspection'][i], properties['hasResponsibleAgent'], ns['dmn'][outputs['hasResponsibleAgent'][i]]),
-        (outputs['hasApprover'][i], RDF.type, classes['agent']),
-        (outputs['hasInspection'][i], properties['hasApprover'], ns['dmn'][outputs['hasApprover'][i]]),
-        (outputs['hasInspection'][i], properties['hasFrequency'], Literal(outputs.get('hasFrequency')[i], datatype=XSD.string)),
-        (outputs['hasInspection'][i], properties['hasStartDate'], Literal(outputs.get('hasInspectionStartDate')[i].strip('/"'), datatype=XSD.dateTime)),
-        (outputs['hasInspection'][i], properties['hasEndDate'], Literal(outputs.get('hasInspectionEndDate')[i].strip('/"'), datatype=XSD.dateTime)),
-        (outputs['hasLocation'][i], RDF.type, classes['location']),
-        (outputs['hasInspection'][i], properties['hasLocation'], ns['dmn'][outputs['hasLocation'][i]]),
-        (outputs['hasInspectionProcedure'][i], RDF.type, classes['inspection_procedure']),
-        (outputs['hasInspection'][i], properties['hasInspectionProcedure'], ns['dmn'][outputs['hasInspectionProcedure'][i]]),
-    ]
-
-    mat = inputs.get('material', [])[i]
-    if mat.lower() == 'concrete':
-        triples.extend([
-            (mat, RDF.type, classes['composite_material']),
-            (mat, properties['hasActivity'], ns['dmn'][inputs['activity'][i]]),
-        ])
-    else:
-        triples.extend([
-            (mat, RDF.type, classes['material']),
-            (mat, properties['hasActivity'], ns['dmn'][inputs['activity'][i]]),
-        ])
-
-    for subject, predicate, obj in triples:
-        dmn_Abox.add((ns['dmn'][subject], predicate, obj))
+def create_individuals(df, graph, ns, classes):
+    for index, row in df.iterrows():
+        for column in df.columns:
+            if column in classes:
+                individual_value = row[column]
+                individual_uri = URIRef(f"{ns['dmn']}{individual_value}")
+                class_uri = classes[column]
+                graph.add((individual_uri, RDF.type, class_uri))
+            
 
 
-    return dmn_Abox
+def link_individuals(df, graph, ns, relationships, literals=None):
+    if literals is None:
+        literals = {}
+    
+    for index, row in df.iterrows():
+        for relationship, related_columns in relationships.items():
+            subject_column, object_column = related_columns
+            subject_value = row[subject_column]
+            subject_uri = URIRef(f"{ns['dmn']}{subject_value}")
+
+            if object_column in literals:
+                object_value = row[object_column]
+                object_datatype = literals[object_column]
+                object_literal = Literal(object_value, datatype=object_datatype)
+                graph.add((subject_uri, relationship, object_literal))
+            else:
+                object_value = row[object_column]
+                object_uri = URIRef(f"{ns['dmn']}{object_value}")
+                graph.add((subject_uri, relationship, object_uri))
+    
+    return graph
+
